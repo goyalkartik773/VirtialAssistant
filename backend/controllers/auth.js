7d13affa6f6bc8aa33f58690e5c881137d3875d8
goyalkartik773 <goyalkartik773@gmail.com>
2025-08-10 22:58:00 +0530

    Update auth.js

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -28,8 +28,8 @@ const signUp = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "lax",
-            secure: false,
+            sameSite: "none",
+            secure: true,
         });
 
         return res.status(200).json({
@@ -76,8 +76,8 @@ const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "strict",
-            secure: false,
+            sameSite: "none",
+            secure: true,
         });
 
         return res.status(200).json({
